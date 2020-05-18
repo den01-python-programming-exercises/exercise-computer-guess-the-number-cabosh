750e891200af9ee9e1bfd8695bdf5916997348be
cabosh <callum.bosher@gmail.com>
2020-05-18 15:42:50 +0100

    Attempted commit

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,29 @@
+from random import randint
+
 def main():
-    #write your code below this line
+
+    print("I'm going to try and guess your number!")
+
+    high = 10
+    low = 1
+
+    while True:
+
+        guess = randint(low,high)
+        user = input("Is " + str(guess) + " your number? ")
+
+        if user == str("yes"):
+            print("That's the correct answer!")
+            break
+
+        elif user == str("no"):
+            highlow = input("Is your number higher or lower than " + str(guess) + "? ")
+
+        if highlow == str("higher"):
+            low = guess + 1
+
+        elif highlow == str("lower"):
+            high = guess - 1
 
 if __name__ == '__main__':
     main()
